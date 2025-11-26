520d12d3d0ef610d76a407f286cdb42469b5dc65
hojun-lang <milky9925@gmail.com>
2025-11-26 16:02:55 +0900

    리드미 변경

---
+++ src/main/java/com/loadingking/loading_king/LoadingKingApplication.java
@@ -0,0 +1,13 @@
+package com.loadingking.loading_king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadingKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadingKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loadingking/loading_king/LoadingKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadingking.loading_king;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadingKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
