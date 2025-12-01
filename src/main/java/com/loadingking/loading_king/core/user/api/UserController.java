7eb19e069548ba03be4f6a99f2d15f333724e1d9
woongblack <woongblack123@gmail.com>
2025-12-01 20:23:26 +0900

    " feat : user controller update "

--- src/main/java/com/loadingking/loading_king/core/user/api/UserController.java
+++ src/main/java/com/loadingking/loading_king/core/user/api/UserController.java
@@ -1,10 +1,13 @@
 package com.loadingking.loading_king.core.user.api;
 
 import com.loadingking.loading_king.core.user.application.UserService;
+import com.loadingking.loading_king.core.user.domain.User;
 import com.loadingking.loading_king.core.user.dto.UserRequest;
 import com.loadingking.loading_king.core.user.dto.UserResponse;
+import com.loadingking.loading_king.infra.security.CustomUserDetail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -13,20 +16,27 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
 
     // TODO: 시큐리티(@AuthenticationPrincipal)에서 유저 정보 꺼내서 넘기기
     @GetMapping("/me")
-    public ResponseEntity<UserResponse> getMyInfo() {
+    public ResponseEntity<UserResponse> getMyInfo(@AuthenticationPrincipal CustomUserDetail userDetail) {
 
-        return ResponseEntity.ok().build();
+        User user = userDetail.getUser();
+        UserResponse response = userService.getMyInfo(user.getId());
+        return ResponseEntity.ok(response);
     }
 
     // TODO: 시큐리티(@AuthenticationPrincipal)에서 유저 정보 꺼내서 넘기기
     @PatchMapping("/me")
-    public ResponseEntity<UserResponse> updateMyInfo(@RequestBody UserRequest request) {
+    public ResponseEntity<UserResponse> updateMyInfo(
+            @AuthenticationPrincipal CustomUserDetail userDetail,
+            @RequestBody UserRequest request) {
 
-        return ResponseEntity.ok().build();
+        User user = userDetail.getUser();
+        UserResponse response = userService.updateMyInfo(user.getId(), request);
+
+        return ResponseEntity.ok(response);
     }
 }
