712c6f09f82f410549bef7791439e15047cd98de
woongblack <woongblack123@gmail.com>
2025-12-01 19:25:27 +0900

    " feat : user controller,entity, service create"

--- src/main/java/com/loadingking/loading_king/core/user/api/UserController.java
+++ src/main/java/com/loadingking/loading_king/core/user/api/UserController.java
@@ -1,4 +1,32 @@
 package com.loadingking.loading_king.core.user.api;
 
+import com.loadingking.loading_king.core.user.application.UserService;
+import com.loadingking.loading_king.core.user.dto.UserRequest;
+import com.loadingking.loading_king.core.user.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private UserService userService;
+
+
+    // TODO: 시큐리티(@AuthenticationPrincipal)에서 유저 정보 꺼내서 넘기기
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getMyInfo() {
+
+        return ResponseEntity.ok().build();
+    }
+
+    // TODO: 시큐리티(@AuthenticationPrincipal)에서 유저 정보 꺼내서 넘기기
+    @PatchMapping("/me")
+    public ResponseEntity<UserResponse> updateMyInfo(@RequestBody UserRequest request) {
+
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/loadingking/loading_king/core/user/domain/User.java
+++ src/main/java/com/loadingking/loading_king/core/user/domain/User.java
@@ -1,4 +1,62 @@
 package com.loadingking.loading_king.core.user.domain;
 
+
+import com.loadingking.loading_king.core.user.application.UserService;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+
+@Builder
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 public class User {
+    //소셜 로그인하기 때문에 필요없는 password는 제거
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "car_number")
+    private String carNumber;
+
+    @Column(name = "join_status")
+    private String joinStatus;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+
+    public User(Long id, String email, Role role, String carNumber, String joinStatus, LocalDateTime createdAt) {
+        this.email = email;
+        this.role = role;
+        this.carNumber = carNumber;
+        this.joinStatus = joinStatus;
+        this.createdAt = createdAt;
+    }
+
+    public void updateInfo(String carNumber) {
+        if (carNumber != null) {
+            this.carNumber = carNumber;   // 소셜 로그인 , 우리 앱에서 수정할수있는건 차량번호만 가능
+
+        }
+    }
 }
