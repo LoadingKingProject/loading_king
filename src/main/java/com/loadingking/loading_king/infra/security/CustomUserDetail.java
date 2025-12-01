83fa786d4bf9d80cfe61c981d67aac6a0c6b54a1
woongblack <woongblack123@gmail.com>
2025-12-01 20:24:14 +0900

    " feat : CustomUserDetail update "

--- src/main/java/com/loadingking/loading_king/infra/security/CustomUserDetail.java
+++ src/main/java/com/loadingking/loading_king/infra/security/CustomUserDetail.java
@@ -1,4 +1,73 @@
 package com.loadingking.loading_king.infra.security;
 
-public class CustomUserDetail {
+import com.loadingking.loading_king.core.user.domain.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class CustomUserDetail implements UserDetails, OAuth2User {
+
+    private final User user;
+    private final Map<String, Object> attributes;
+
+    public CustomUserDetail(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return user.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+
+    //계정 상태 관리
+    //소셜 로그인만 사용하기 때문에 모두 true로 설정
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
